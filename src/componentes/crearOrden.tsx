8c05b73b295bf2467021d4f7bcc4eeb9795b8b33
Danny Granados <dgranados571@gmail.com>
2024-08-05 23:12:43 -0500

    Ajusta animacion Dettal OP

--- src/componentes/crearOrden.tsx
+++ src/componentes/crearOrden.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { DashBoardProps } from '../interfaces/IAuthServices'
+import construccionSoft from '../img/construccionSoftware.jpg'
 
 const CrearOrden: React.FC<DashBoardProps> = ({ setCargando }) => {
 
     return (
         <>
-            
+            <div className='div-style-form'>
+                <h3 className='titulo-form'>Mi cuenta **en construcción**</h3>
+                <div>
+                    <img className='card-img' src={construccionSoft} style={{objectFit: 'contain'}}  alt=''></img>
+                </div>
+            </div>
         </>
     )
 }
